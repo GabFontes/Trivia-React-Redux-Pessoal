ffda620c2096c8555adeadca3943cf99c6d21738
AirtonL <junynnhoh@gmail.com>
2022-01-13 17:09:52 -0300

    fix lint

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { userLogin } from '../redux/actions';
 import { setLocalStorage, getLocalStorage } from '../services/localStorage';
@@ -14,16 +15,16 @@ class Login extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  async handleClick(){
+  async handleClick() {
     const { history, dispatch } = this.props;
     dispatch(userLogin(this.state));
     const returnToken = await requestToken();
     setLocalStorage('token', returnToken);
     console.log(getLocalStorage());
-    history.push('/trivia')
+    history.push('/trivia');
   }
 
   handleChange({ target: { value, name } }) {
@@ -36,40 +37,45 @@ class Login extends Component {
     const { name, email } = this.state;
     return (
       <>
-      <form>
-        <label htmlFor="email">
-          <input
-            data-testid="input-gravatar-email"
-            onChange={ this.handleChange }
-            value={ email }
-            id="email"
-            type="email"
-            name="email"
-          />
-        </label>
-        <label htmlFor="name">
-          <input
-            data-testid="input-player-name"
-            value={ name }
-            onChange={ this.handleChange }
-            type="text"
-            id="name"
-            name="name"
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="btn-play"
-          disabled={ name.length < 1 || email.length < 1 }
-          onClick={ this.handleClick }
-        >
-          Play
-        </button>
-      </form>
-        <Link to="/settings" type="button" data-testid="btn-settings" >Settings</Link>
+        <form>
+          <label htmlFor="email">
+            <input
+              data-testid="input-gravatar-email"
+              onChange={ this.handleChange }
+              value={ email }
+              id="email"
+              type="email"
+              name="email"
+            />
+          </label>
+          <label htmlFor="name">
+            <input
+              data-testid="input-player-name"
+              value={ name }
+              onChange={ this.handleChange }
+              type="text"
+              id="name"
+              name="name"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ name.length < 1 || email.length < 1 }
+            onClick={ this.handleClick }
+          >
+            Play
+          </button>
+        </form>
+        <Link to="/settings" type="button" data-testid="btn-settings">Settings</Link>
       </>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.instanceOf(Object),
+  dispatch: PropTypes.func,
+}.isRequired;
 
 export default connect()(Login);
--- src/services/requestAPI.js
+++ src/services/requestAPI.js
@@ -1,7 +1,7 @@
-const REQUEST_TOKEN = 'https://opentdb.com/api_token.php?command=request'
+const REQUEST_TOKEN = 'https://opentdb.com/api_token.php?command=request';
 
 const requestToken = () => fetch(REQUEST_TOKEN)
-    .then((response) => response.json())
-    .then((data) => data.token);
+  .then((response) => response.json())
+  .then((data) => data.token);
 
 export default requestToken;
